16bc5f54a3a0214e2e5b7205808741a714f1e1a0
lemosjuliana <lemos.julianajesus@gmail.com>
2023-01-14 00:19:32 -0300

    add-cart-broken

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,12 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const tents = getLocalStorage("so-cart");
+  tents.push(product);
+  setLocalStorage("so-cart", tents);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
