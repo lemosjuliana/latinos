46e8c7a1801e6346be7b72b3fdc0a4336988b781
andersoncaroline <gra23005@byui.edu>
2023-02-09 18:41:49 -0500

    team 5

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -5,7 +5,7 @@ function productCardTemplate(product) {
   return `<li class="product-card">
   <a href="product_pages/index.html?product=${product.Id}">
   <img
-    src="${product.Image}"
+    src="${product.Images.PrimaryMedium}"
     alt="Image of ${product.Name}"
   />
   <h3 class="card__brand">${product.Brand.Name}</h3>
@@ -24,9 +24,11 @@ export default class ProductListing {
   }
   async init() {
     // our dataSource will return a Promise...so we can use await to resolve it.
-    const list = await this.dataSource.getData();
+    const list = await this.dataSource.getData(this.category);
     // render the list 
     this.renderList(list);
+    //set the title to the current category
+    document.querySelector(".product-title").innerHTML = this.category;
   }
   renderList(list) {
     renderListWithTemplate(productCardTemplate, this.listElement, list);
--- src/js/cart.js
+++ src/js/cart.js
@@ -1,4 +1,4 @@
-import { getLocalStorage, loadHeaderFooter } from "./utils.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
 import ShoppingCart from "./ShoppingCart.js";
 
 const cart = new ShoppingCart("so-cart", ".product-list");
